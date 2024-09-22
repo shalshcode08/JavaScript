9726cdaccb7f79abad43615758afb2eb92fce0d1
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-23 00:58:22 +0530

    Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,3 +23,33 @@ function loginUserMessage (username = "somu") { //hard coded parameters
 }
 
 console.log(loginUserMessage()) //if we dosent pass any value then it return undefined
+
+function calculateCartPrice (val1, ...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 300 , 400));
+
+
+//rest operator ... || spread operator
+// this will help in pass multiple parameter user ... operator(spread)
+
+const user = {
+    username : "somu",
+    price : 199
+}
+
+function handleObject (anyobject){
+    console.log(`username is ${anyobject.username}`);
+    console.log(`price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+
+const myarr = [100,200,300]
+function returnArray(mynewarr){
+    return mynewarr;
+}
+
+console.log(returnArray(myarr));
