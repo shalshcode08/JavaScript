26af54755c544195b78f152812bfdb9c85579cf2
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-22 18:07:10 +0530

    Operations

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -1,8 +1,27 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score); 
+// console.log(typeof score); 
 
-let valueInNumber = Number(score)
-// this will gurantee that the score is of type number
-console.log(typeof score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// // this will gurantee that the score is of type number
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+
+// *******OPERATIONS*********
+
+let val1 = 3;
+let negValue = -val1
+console.log(val1);
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "somu"
+let str2 = "shrestha"
+let str3 = str1 + " " + str2
+console.log(str3);
