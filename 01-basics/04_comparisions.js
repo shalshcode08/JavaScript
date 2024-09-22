b5ee2708282d01cf3db173758934ab54aefc324b
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-22 18:12:30 +0530

    Compartsions

---
+++ 01-basics/04_comparisions.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false Null is converted to 0 and then is checked
+console.log(null>=0); //true
+
