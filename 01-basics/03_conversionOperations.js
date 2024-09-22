f0d6fcb0b7c6cfb27e7e8acc1e3dae1014409fef
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-22 18:02:35 +0530

    Conversion

---
+++ 01-basics/03_conversionOperations.js
@@ -0,0 +1,8 @@
+let score = "33"
+
+console.log(typeof score); 
+
+let valueInNumber = Number(score)
+// this will gurantee that the score is of type number
+console.log(typeof score);
+console.log(typeof valueInNumber);
