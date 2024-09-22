e2ed3a996c7313ed5300f746f6b646666b0af10e
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-22 18:19:45 +0530

    Strings

---
+++ 01-basics/05_strings.js
@@ -0,0 +1,11 @@
+const fname = "Somu"
+const lname = " Shrestha"
+
+// console.log(fname + " " + lname + " Hi");
+
+//new and correct way to write
+console.log(`Hi my name is ${fname.toUpperCase() + lname}`);
+
+const getName = new String ('Somya')
+console.log(getName.length);
+console.log(getName.toUpperCase());
