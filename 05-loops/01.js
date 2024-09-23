99f9e5a8a01312efc32ad3f9ab90630acab7d34f
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-23 09:32:48 +0530

    for loop

---
+++ 05-loops/01.js
@@ -0,0 +1,25 @@
+//for loop
+for(let i=0; i<=10; i++){
+    // console.log(i);
+    if(i==5){
+        // console.log("5 is the best number");
+        
+    }
+}
+
+//nested 
+for(let i=1; i<=10; i++){
+    // console.log(`Table for : ${i}`);
+    
+    for(let j=1; j<=10; j++){
+        // console.log(i + ":" + j); 
+        // console.log(i + " * " + j + " = " + i*j);
+        
+    }
+}
+
+
+const myarr = ["somu", "som", "so", "s"]
+for(let i=0; i<myarr.length; i++){
+    console.log(myarr[i]);
+}
